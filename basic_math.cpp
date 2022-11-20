adad82633ccc71f4b108e5f41f2718b4903d11c8
Gintaras <gliaudialis.ga@phystech.edu>
2022-11-21 00:43:40 +0300

    cpp file for class vector3d

---
+++ basic_math.cpp
@@ -0,0 +1,209 @@
+#include "basic_math.hpp"
+#include <cmath>
+
+/*
+    Constructors
+*/
+template<typename T>
+vector3d<T>::vector3d():
+    data(new T[3]) {}
+template<typename T>
+vector3d<T>::vector3d(const T& x_,const T& y_,const T& z_):
+    data(new T[3])
+{
+    data[0] = x_;
+    data[1] = y_;
+    data[2] = z_;
+}
+template<typename T>
+vector3d<T>::vector3d(T* arr):
+    data(arr) {}
+
+/*
+    RAII
+*/
+template<typename T>
+vector3d<T>::vector3d(const vector3d<T>& lhs): data(new T[3])
+{
+    data[0] = lhs.data[0];
+    data[1] = lhs.data[1];
+    data[2] = lhs.data[2];
+}
+template<typename T>
+vector3d<T>& vector3d<T>::operator=(const vector3d& lhs)
+{
+    if (this == &lhs)
+        return *this;
+    vector3d<T> tmp(lhs);
+    std::swap(data, tmp.data);
+    return *this;
+}
+template<typename T>
+vector3d<T>::vector3d(vector3d<T>&& rhs): data(rhs.data)
+{
+    rhs.data = nullptr;
+}
+template<typename T>
+vector3d<T>& vector3d<T>::operator=(vector3d&& rhs)
+{
+    vector3d<T> tmp(std::move(rhs));
+    std::swap(data, tmp.data);
+    return *this;
+}
+template<typename T>
+vector3d<T>::~vector3d()
+{
+    delete[] data;
+}
+
+/*
+    Binary Operators
+*/
+template<typename T>
+bool vector3d<T>::operator==(const vector3d<T>& other)
+{
+    bool f = 1;
+    f &= (data[0] == other.data[0]);
+    f &= (data[1] == other.data[1]);
+    f &= (data[2] == other.data[2]);
+    return f;
+}
+template<typename T>
+bool vector3d<T>::operator!=(const vector3d<T>& other)
+{
+    return !(this->operator==(other));
+}
+template<typename T>
+vector3d<T>& vector3d<T>::operator+(const vector3d<T>& other)
+{
+    vector3d<T>* tmp = new vector3d<T>(data[0]+other.data[0], data[1]+other.data[1], data[2]+other.data[2]);
+    return *tmp;
+}
+template<typename T>
+vector3d<T>& vector3d<T>::operator-(const vector3d<T>& other)
+{
+    vector3d<T>* tmp = new vector3d<T>(data[0]-other.data[0], data[1]-other.data[1], data[2]-other.data[2]);
+    return *tmp;
+}
+template<typename T>
+T& vector3d<T>::operator*(const vector3d<T>& other)
+{
+    T* res = new T;
+    *res = data[0] * other.data[0] + data[1] * other.data[1] + data[2] * other.data[2];
+    return *res;
+}
+template<typename T>
+vector3d<T>& vector3d<T>::operator^(const vector3d<T>& other)
+{
+    vector3d<T>* tmp = new vector3d<T>;
+    tmp->data[0] = data[1] * other.data[2] - data[2] * other.data[1];
+    tmp->data[1] = data[2] * other.data[0] - data[0] * other.data[2];
+    tmp->data[2] = data[0] * other.data[1] - data[1] * other.data[0];
+    return *tmp;
+}
+template<typename T>
+vector3d<T>& vector3d<T>::operator*(const T& mult)
+{
+    vector3d<T>* tmp = new vector3d<T>(data[0] * mult, data[1] * mult, data[2] * mult);
+    return *tmp;
+}
+template<typename T1, typename T2>
+vector3d<T2>& operator*(const T1& m, const vector3d<T2>& v)
+{
+    vector3d<T2>* tmp = new vector3d<T2>(v.x() * m, v.y() * m, v.z() * m);
+    return *tmp;
+}
+
+/*
+    Unary Operators
+*/
+template<typename T>
+vector3d<T>& vector3d<T>::operator +=(const vector3d& other)
+{
+    data[0] += other.data[0];
+    data[1] += other.data[1];
+    data[2] += other.data[2];
+    return *this;
+}
+template<typename T>
+vector3d<T>& vector3d<T>::operator -=(const vector3d& other)
+{
+    data[0] -= other.data[0];
+    data[1] -= other.data[1];
+    data[2] -= other.data[2];
+    return *this;
+}
+template<typename T>
+vector3d<T>& vector3d<T>::operator *=(const T& mult)
+{
+    data[0] *= mult;
+    data[1] *= mult;
+    data[2] *= mult;
+    return *this;
+}
+template<typename T>
+vector3d<T>& vector3d<T>::operator ^=(const vector3d& other)
+{
+    vector3d<T> tmp;
+    tmp.data[0] = data[1] * other.data[2] - data[2] * other.data[1];
+    tmp.data[1] = data[2] * other.data[0] - data[0] * other.data[2];
+    tmp.data[2] = data[0] * other.data[1] - data[1] * other.data[0];
+    this->operator=(tmp);
+    return *this;
+}
+template<typename T>
+vector3d<T>& vector3d<T>::operator -()
+{
+    vector3d<T>* tmp = new vector3d<T>(-data[0], -data[1], -data[2]);
+    return *tmp;
+}
+
+/*
+    Methods
+*/
+template<typename T>
+T& vector3d<T>::x()
+{
+    return data[0];
+}
+template<typename T>
+T& vector3d<T>::y()
+{
+    return data[1];
+}
+template<typename T>
+T& vector3d<T>::z()
+{
+    return data[2];
+}
+template<typename T>
+T vector3d<T>::x() const
+{
+    return data[0];
+}
+template<typename T>
+T vector3d<T>::y() const
+{
+    return data[1];
+}
+template<typename T>
+T vector3d<T>::z() const
+{
+    return data[2];
+}
+template<typename T>
+T vector3d<T>::len()
+{
+    return sqrt(this->operator*(*this));
+}
+template<typename T>
+void vector3d<T>::print()
+{
+    std::cout << "(" << data[0] << ", " << data[1] << ", " << data[2] << ")\n";
+    return;
+}
+template<typename T>
+std::ostream & operator <<(std::ostream& os, const vector3d<T>& v)
+{
+    return os << "(" << v.data[0] << ", " << v.data[1] << ", " << v.data[2] << ")\n"; 
+}
